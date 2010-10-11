ac96ee08a0ba58cab552f1cdd20e9f4fa581af36
Arthur Debert <arthur@stimuli.com.br>
2010-10-11 12:49:30 -0300

    changed readme path to fix distutils setup.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from adminmodelaction import VERSION
 
 
-f = open(os.path.join(os.path.dirname(__file__), 'README.txt'))
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 readme = f.read()
 f.close()
 
